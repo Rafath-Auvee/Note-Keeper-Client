f5a33042fa196e72f56ca972939ae48c9a04db11
Rafath-Bin-Zafar-Auvee <rbza98@outlook.com>
2022-09-13 10:21:47 +0600

    draft pinned post

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -17,13 +17,19 @@ const Home = () => {
   const firstPostIndex = lastPostIndex - postsPerPage;
   const currentPosts = todos.slice(firstPostIndex, lastPostIndex);
 
+  const [sample, setSample] = useState();
+
   useEffect(() => {
     fetch(`${baseURL}`)
       .then((res) => res.json())
       .then((data) => {
         setTodo(data);
       });
-  }, [todos]);
+  }, []);
+  // useEffect( () => {
+
+  // }, []);
+  
 
   const editTodo = async (todo) => {
     console.log(todo);
@@ -33,8 +39,17 @@ const Home = () => {
     await navigate(`/note/${todo._id}`, { state: todo });
   };
   const handlePinned = async (todo, array, from, to) => {
-    console.log(array)
-    return array.splice(to, 0, array.splice(from, 1)[0])
+    
+    console.log("array");
+    console.log(array);
+    let newCart = [];
+    // const exists = todos.find(note => note._id === todo._id);
+    const exists = array.splice(to, 0, array.splice(from, 1)[0]);
+    // const rest = todos.filter(note => note._id !== todo._id);
+    console.log()
+
+    setTodo(array);
+
   };
 
   const confirmDelete = async (id) => {
@@ -66,6 +81,51 @@ const Home = () => {
     }).then((res) => res.json().then((data) => {}));
   };
 
+  const sampleMap = (
+    <>
+      {currentPosts.map((todo, index, arr) => (
+        <div
+          key={index}
+          className="card w-[350px] bg-neutral text-primary-content"
+        >
+          <div className="card-body">
+            <h2 className="card-title">
+              {index + 1} - {todo.Task}
+            </h2>
+
+            <p>{todo.tagline}</p>
+            <div className="card-actions justify-end">
+              <button
+                onClick={() => handlePinned(todo, arr, index, 0)}
+                className="btn btn-outline bg-green-500 text-white btn-xs"
+              >
+                Pinned
+              </button>
+              <button
+                onClick={() => navigateNote(todo._id)}
+                className="btn btn-outline bg-blue-500 text-white btn-xs"
+              >
+                Read More
+              </button>
+              <button
+                className="btn btn-outline bg-amber-500 text-white btn-xs"
+                onClick={() => editTodo(todo)}
+              >
+                Edit
+              </button>
+              <button
+                className="btn btn-outline bg-red-500 text-white btn-xs"
+                onClick={() => confirmDelete(todo._id)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+
   const handleError = (id) => {
     const agree = window.confirm("Not Complete?");
     const url = `http://localhost:5000/all/${id}`;
@@ -81,44 +141,7 @@ const Home = () => {
   return (
     <div className=" mx-[100px] my-[100px]">
       <div className="grid justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[50px]">
-        {currentPosts.map((todo, index, arr) => (
-          <div
-            key={index}
-            className="card w-[350px] bg-neutral text-primary-content"
-          >
-            <div className="card-body">
-              <h2 className="card-title">{index+1} - {todo.Task}</h2>
-
-              <p>{todo.tagline}</p>
-              <div className="card-actions justify-end">
-                <button
-                  onClick={() => handlePinned(todo, arr, index, 0 )}
-                  className="btn btn-outline bg-green-500 text-white btn-xs"
-                >
-                  Pinned
-                </button>
-                <button
-                  onClick={() => navigateNote(todo._id)}
-                  className="btn btn-outline bg-blue-500 text-white btn-xs"
-                >
-                  Read More
-                </button>
-                <button
-                  className="btn btn-outline bg-amber-500 text-white btn-xs"
-                  onClick={() => editTodo(todo)}
-                >
-                  Edit
-                </button>
-                <button
-                  className="btn btn-outline bg-red-500 text-white btn-xs"
-                  onClick={() => confirmDelete(todo._id)}
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          </div>
-        ))}
+        {sampleMap}
       </div>
       <div className="grid justify-items-center mt-[100px]">
         <Pagination
